78a43019b44d219b7acdb0f5c86d8abf9ce50da7
rkant18 <k46690968@gmail.com>
2024-05-11 20:45:09 +0100

    asa

--- Modules/main.tf
+++ Modules/main.tf
@@ -11,26 +11,26 @@ module "vnet" {
   azurerm_virtual_network = var.module_infra
 
 }
-### To create new subnet for virual network to use in resources
-module "subnet" {
-  depends_on = [module.vnet]
-  source     = "github.com/rkant18/manual-code.git/Infra/subnet"
-  subnet     = var.module_subnet
-}
+# ### To create new subnet for virual network to use in resources
+# module "subnet" {
+#   depends_on = [module.vnet]
+#   source     = "github.com/rkant18/manual-code.git/Infra/subnet"
+#   subnet     = var.module_subnet
+# }
 
-### To create new network interface cards for VM use
-module "nic" {
-  depends_on = [module.subnet]
-  source     = "../Infra/interface"
-  nic        = var.module_nic
+# ### To create new network interface cards for VM use
+# module "nic" {
+#   depends_on = [module.subnet]
+#   source     = "../Infra/interface"
+#   nic        = var.module_nic
 
-}
-#### TO create Linux vm 
-module "lnx-vm" {
-  source   = "../Infra/vm-linux-instance"
-  linux-vm = var.module_vm
-}
-#### TO create sql-db server and database 
-module "sqlserver" {
-  source = "../Infra/sqldb"
-}
+# }
+# #### TO create Linux vm 
+# module "lnx-vm" {
+#   source   = "../Infra/vm-linux-instance"
+#   linux-vm = var.module_vm
+# }
+# #### TO create sql-db server and database 
+# module "sqlserver" {
+#   source = "../Infra/sqldb"
+# }
